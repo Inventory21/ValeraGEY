05c3eaec03be5d29bb4a2451575661dd6fe7158a
Inventory21 <centrcoteyka@gmail.com>
2025-08-18 00:23:16 +0300

    Решение задачи 13

--- main.cpp
+++ main.cpp
@@ -8,16 +8,13 @@ int main() {
     std::cout << " X    |    Y" << std::endl;
     std::cout << "-------------------" << std::endl;
 
-    for (x = -2.0; x <= 2.0; x += 0.5) {
-        if (x <= -1) {
-            y = 1;
-        } else if (x <= 1) {
-            y = -x;
-        } else {
-            y = -1;
-        }
-
-        std::cout << std::fixed << std::setprecision(2) << std::setw(6) << x << " | " << std::setw(6) << y << std::endl;
+    for (x = -1.5; x <= 1.5; x += 0.1) {
+        if (x <= -1) 
+        {y = 1;}
+        else if (x <= 1) 
+        {y = -x;} 
+        else {y = -1;}
+        std::cout << std::fixed << std::setprecision(1) << std::setw(6) << x << " | " << std::setw(6) << y << std::endl;
     }
 return 0;
 }
