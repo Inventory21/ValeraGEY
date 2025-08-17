2cddd0100b5e2413b21c3a38c1fdb147d588b5fd
Inventory21 <centrcoteyka@gmail.com>
2025-08-17 23:44:09 +0300

    Решение задачи 7

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 #include <iomanip>
 
 int main() {
-    double x, y;
-
-    std::cout << "   x      |    y(x)" << std::endl;
-    std::cout << "------------------------" << std::endl;
-
-    for (x = -4.0; x <= 4.0; x += 0.5) {
-        y = 0.5 * x * x - 7 * x;
-        std::cout << std::fixed << std::setprecision(1) << std::setw(7) << x << " | " << std::setw(7) << y << std::endl;
+    int n = 6;
+    int factorial = 1;
+    for (int i = 1; i <= n; ++i) {
+        factorial *= i;
     }
+
+    std::cout << "Factorial " << n << " is " << factorial << std::endl;
 return 0;
 }
