a452fa8512dd368861259454ddd430d9c37f996e
Inventory21 <centrcoteyka@gmail.com>
2025-08-17 14:58:02 +0300

    zalupa1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int s = 0, i ;
+    for (i = 2; i <= 35; i = i + 3){
+    s = s + i; 
+    }
+ 
+    std::cout << "s = " << s << std::endl;
+
+return 0;
+}
